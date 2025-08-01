94668b65be7b2bad456667b4f50b72ab592aa6ef
UzairAtiq <uzairatiq65@gmail.com>
2025-08-01 19:05:40 +0500

    third

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express"); // Common JS
 const app = express(); // Used for all funcitons
 const path = require('path');
 
-app.get("/", (req, res) => {
-  // to define get behaviour on root directory
-  // res.send("<h1>Hello world!<h1/>")
-  res.send({ message: "Hello world" }); // We do not need to manually set content type headers in express 
-});
-app.get("/about",(req,res)=>{
-  res.sendFile(path.join(__dirname,'public','index.html')) // to send a file in express
-})
+// app.get("/", (req, res) => {
+//   // to define get behaviour on root directory
+//   // res.send("<h1>Hello world!<h1/>")
+//   res.send({ message: "Hello world" }); // We do not need to manually set content type headers in express 
+// });
+// app.get("/about",(req,res)=>{
+//   res.sendFile(path.join(__dirname,'public','index.html')) // to send a file in express
+// })
+// ---- The above method to send static files is too junky , instead we can use .static to decalre a folder as static and express will automatically send the files inside that folder as static files 
+
+app.use(express.static(path.join(__dirname,'public')))
+
 app.listen(8000, () => console.log("Server running on port 8000 OK")); // Listening for request on port 8000
